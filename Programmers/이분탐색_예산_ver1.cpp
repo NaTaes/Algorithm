dade9a315d8939261179be59106d6ed6e0a178f6
NaTaes <30484055+NaTaes@users.noreply.github.com>
2019-10-08 14:30:55 +0900

    Add files via upload

---
+++ Programmers/이분탐색_예산_ver1.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct cmp
+{
+    bool operator()(int a, int b)
+    {
+        return a < b;
+    }
+}mycmp;
+
+int solution(vector<int> budgets, int M) {
+    int answer = 0;
+
+    sort(budgets.begin(), budgets.end(), mycmp);
+
+    int myMax;
+
+    for(int i=0; i<budgets.size(); i++)
+    {
+        myMax = M / (budgets.size() - i);
+        if(myMax < budgets[i])
+        {
+            answer = myMax;
+            break;
+        }
+        M -= budgets[i];
+    }
+
+    if(answer == 0)
+    {
+        answer = budgets[budgets.size() - 1];
+    }
+
+    return answer;
+}
